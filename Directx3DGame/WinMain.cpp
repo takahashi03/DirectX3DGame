32b0c488b5b508f65e857c0875b3c76ddca16c1e
TakahashiChisato <takahashi.k017a1654@gmail.com>
2019-06-17 17:04:59 +0900

    winMainをAppでのループに変更

--- Directx3DGame/WinMain.cpp
+++ Directx3DGame/WinMain.cpp
@@ -1,32 +1,14 @@
-#include "Window.h"
+#include "App.h"
 
-// エントリポイント
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
-	LPSTR     IpCmdLine,
+	LPSTR     lpCmdLine,
 	int       nCmdShow)
 {
 	try
 	{
-		// Windowをインスタンス化
-		Window window(800, 300, "Winodw");
-
-		// メッセージループ
-		MSG msg;
-		BOOL gResult;
-		while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-
-		if (gResult == 1)
-		{
-			return -1;
-		}
-
-		return msg.wParam;
+		return App{}.Go();
 	}
 	catch (const MyException& e)
 	{
