21e0f1b02962fed1bb6941f4f189903b5c56a961
TakahashiChisato <takahashi.k017a1654@gmail.com>
2019-06-24 12:48:44 +0900

    球体、三角柱、角柱の描画

---
+++ Directx3DGame/Cone.h
@@ -0,0 +1,64 @@
+#pragma once
+#include "IndexedTriangleList.h"
+#include <DirectXMath.h>
+#include "MyMath.h"
+
+class Cone
+{
+public:
+	template <class V>
+	static IndexedTriangleList<V> MakeTesselated(int longDiv)
+	{
+		namespace dx = DirectX;
+		assert(longDiv >= 3);
+
+		const auto base = dx::XMVectorSet(1.0f, 0.0f, -1.0f, 0.0f);
+		const float longitudeAngle = 2.0f * PI / longDiv;
+
+		// 頂点
+		std::vector<V> vertices;
+		for (int iLong = 0; iLong < longDiv; iLong++)
+		{
+			vertices.emplace_back();
+			auto v = dx::XMVector3Transform(
+				base,
+				dx::XMMatrixRotationZ(longitudeAngle * iLong)
+			);
+			dx::XMStoreFloat3(&vertices.back().pos, v);
+		}
+		// 中心
+		vertices.emplace_back();
+		vertices.back().pos = { 0.0f,0.0f, -1.0f };
+		const auto iCenter = (unsigned short)(vertices.size() - 1);
+
+		// 先端
+		vertices.emplace_back();
+		vertices.back().pos = { 0.0f,0.0f, 1.0f };
+		const auto iTip = (unsigned short)(vertices.size() - 1);
+
+		// インデックス
+		std::vector<unsigned short> indices;
+		for (unsigned short iLong = 0; iLong < longDiv; iLong++)
+		{
+			indices.push_back(iCenter);
+			indices.push_back((iLong + 1) % longDiv);
+			indices.push_back(iLong);
+		}
+
+		// コーンインデックス
+
+		for (unsigned short iLong = 0; iLong < longDiv; iLong++)
+		{
+			indices.push_back(iLong);
+			indices.push_back((iLong + 1) % longDiv);
+			indices.push_back(iTip);
+		}
+
+		return { std::move(vertices),std::move(indices) };
+	}
+	template<class V>
+	static IndexedTriangleList<V> Make()
+	{
+		return MakeTesselated<V>(24);
+	}
+};
