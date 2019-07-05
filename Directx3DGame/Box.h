3affc624962c6c7d47b762e87175842a30d62399
TakahashiChisato <takahashi.k017a1654@gmail.com>
2019-07-05 14:58:43 +0900

    テストオブジェクトの作成

--- Directx3DGame/Box.h
+++ Directx3DGame/Box.h
@@ -1,7 +1,7 @@
 #pragma once
-#include "DrawableBase.h"
+#include "TestObject.h"
 
-class Box : public DrawableBase<Box>
+class Box : public TestObject<Box>
 {
 public:
 	Box(Graphics& gfx, std::mt19937& rng,
@@ -11,24 +11,7 @@ public:
 		std::uniform_real_distribution<float>& rdist,
 		std::uniform_real_distribution<float>& bdist,
 		DirectX::XMFLOAT3 material);
-	void Update(float dt) noexcept override;
 	DirectX::XMMATRIX GetTransformXM() const noexcept override;
 private:
-	// ’èˆÊ’u
-	float r;
-	float roll = 0.0f;
-	float pitch = 0.0f;
-	float yaw = 0.0f;
-	float theta;
-	float phi;
-	float chi;
-	// ‘¬“x(delta/s)
-	float droll;
-	float dpitch;
-	float dyaw;
-	float dtheta;
-	float dphi;
-	float dchi;
-	// ƒ‚ƒfƒ‹transform
 	DirectX::XMFLOAT3X3 mt;
 };
